ba780a37bd55dd54f28b90930caa216f4a5bfe95
davidbot013 <david.cortes@correounivalle.edu.co>
2020-05-13 11:27:13 -0500

    Counting sort.

--- soluciones/lineal/aux.py
+++ soluciones/lineal/aux.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
+import copy
+# Implementación de Counting Sort
+# Ordena una lista de tuplas en orden ascendente en O(n).
+# Cada tupla tiene la forma (llave:valor) valor es un entero de 0 a k
+def sort(lista, k):
+    C = [0 for i in range(k)]
+    B = [None for i in range(len(lista))] 
+    
+    for j in range(len(lista)):
+        indice = lista[j][1] - 1
+        C[indice] = C[indice] + 1
 
-# Ordena una tupla en orden ascendente.
-def sort(tupla):
+    for i in range(2,k):
+        C[i] = C[i] + C[i-1]
+    
+    for j in range(len(lista)-1, -1,-1):
+        indice = lista[j][1] - 1 # Hace las veces de C[A[j]] en el pseudocódigo del libro.
+        B[indice] = lista[j]
+        C[indice] -= 1
+    
+    return B
--- soluciones/lineal/lineal.py
+++ soluciones/lineal/lineal.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys, json
+import aux
 
 N = 0
 M = 0
@@ -8,13 +9,13 @@ K = 0
 animales = []
 grandezas = []
 partes = []
+animales_y_grandezas = []
 # Se encarga de "traducir" cada línea del archivo entrada
 # a tipos de datos trabajables.
 def procesar_entrada(archivo):
     with open(archivo) as _input:
-        lineas = _input.read()
+        lineas = _input.readlines()
     
-    lineas = lineas.splitlines()
     N,M,K = int(lineas[0]), int(lineas[1]), int(lineas[2])
     
     animales = json.loads(lineas[3])
@@ -23,5 +24,8 @@ def procesar_entrada(archivo):
     for i in range(5, (M-1)+6):
         _parte = json.loads(lineas[i])
         partes.append(_parte)
+
+    animales_y_grandezas = list(zip(animales, grandezas)) # Lista de tuplas (animal:grandeza)
+    animales_y_grandezas = aux.sort(animales_y_grandezas, N)
 
 procesar_entrada(sys.argv[1])
