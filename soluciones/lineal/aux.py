6080fa8cbf8dc704a69472b59efbf83937002942
davidbot013 <david.cortes@correounivalle.edu.co>
2020-05-12 22:11:29 -0500

    Avance en la solución lineal.

---
+++ soluciones/lineal/aux.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+# Ordena una tupla en orden ascendente.
+def sort(tupla):
--- soluciones/lineal/lineal.py
+++ soluciones/lineal/lineal.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python3
-import sys
+import sys, json
 
-with open(sys.argv[1]) as _input:
-    linea = _input.readline()
+N = 0
+M = 0
+K = 0
+
+animales = []
+grandezas = []
+partes = []
+# Se encarga de "traducir" cada línea del archivo entrada
+# a tipos de datos trabajables.
+def procesar_entrada(archivo):
+    with open(archivo) as _input:
+        lineas = _input.read()
+    
+    lineas = lineas.splitlines()
+    N,M,K = int(lineas[0]), int(lineas[1]), int(lineas[2])
+    
+    animales = json.loads(lineas[3])
+    grandezas = json.loads(lineas[4])
+    
+    for i in range(5, (M-1)+6):
+        _parte = json.loads(lineas[i])
+        partes.append(_parte)
+
+procesar_entrada(sys.argv[1])
